ee12ee19f22590fb94e6997242c7082f78fbfcee
cesarparra26 <cesarparra26@gmail.com>
2018-06-13 17:26:18 -0400

    añadimos login y logout

--- src/app/componentes/login-page/login-page.component.ts
+++ src/app/componentes/login-page/login-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AuthService } from '../../servicios/auth.service';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPageComponent implements OnInit {
 
-  constructor() { }
+  public email: string;
+  public password: string;
+
+  constructor(
+    public authService: AuthService,
+    public router: Router
+  ) { }
 
   ngOnInit() {
+  }
+
+  onSubmitLogin() {
+    this.authService.loginEmail(this.email, this.password)
+    .then( (res) => {
+      this.router.navigate(['/privado']);
+    }).catch((err) => {
+        console.log(err);
+        this.router.navigate(['/login']);
+    });
   }
 
 }
