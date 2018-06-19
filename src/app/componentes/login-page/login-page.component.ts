eac84e7533d8bbe14cc3310560bf6cb848504a05
cesarparra26 <cesarparra26@gmail.com>
2018-06-19 14:15:47 -0400

    añadimos google login

--- src/app/componentes/login-page/login-page.component.ts
+++ src/app/componentes/login-page/login-page.component.ts
@@ -32,4 +32,11 @@ export class LoginPageComponent implements OnInit {
     });
   }
 
+  onclickGoogleLogin() {
+    this.authService.loginGoogle()
+    .then((res) => {
+      this.router.navigate(['/privado']);
+    }).catch( err => console.log(err.message));
+  }
+
 }
